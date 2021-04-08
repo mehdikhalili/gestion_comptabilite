78df0738ef56d24e8e3d252b589fdb858ed3b34e
Mehdi Khalili <mehdikhalili860@gmail.com>
2021-04-09 00:57:57 +0100

    Create BankAccount entity

---
+++ src/Entity/BankAccount.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BankAccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BankAccountRepository::class)
+ */
+class BankAccount
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bankName;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $solde;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numero;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBankName(): ?string
+    {
+        return $this->bankName;
+    }
+
+    public function setBankName(string $bankName): self
+    {
+        $this->bankName = $bankName;
+
+        return $this;
+    }
+
+    public function getSolde(): ?float
+    {
+        return $this->solde;
+    }
+
+    public function setSolde(float $solde): self
+    {
+        $this->solde = $solde;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+}
