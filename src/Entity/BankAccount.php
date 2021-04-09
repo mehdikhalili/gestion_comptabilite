ab335da124f34abb889f40e42c6321eb4fd6d6cf
Mehdi Khalili <mehdikhalili860@gmail.com>
2021-04-09 16:18:15 +0100

    Add Transaction entity and link it with BankAccount

--- src/Entity/BankAccount.php
+++ src/Entity/BankAccount.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BankAccountRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,16 @@ class BankAccount
      * @ORM\Column(type="string", length=255)
      */
     private $numero;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Transaction::class, mappedBy="bankAccount", orphanRemoval=true)
+     */
+    private $transactions;
+
+    public function __construct()
+    {
+        $this->transactions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +81,36 @@ class BankAccount
     public function setNumero(string $numero): self
     {
         $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Transaction[]
+     */
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
+
+    public function addTransaction(Transaction $transaction): self
+    {
+        if (!$this->transactions->contains($transaction)) {
+            $this->transactions[] = $transaction;
+            $transaction->setBankAccount($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransaction(Transaction $transaction): self
+    {
+        if ($this->transactions->removeElement($transaction)) {
+            // set the owning side to null (unless already changed)
+            if ($transaction->getBankAccount() === $this) {
+                $transaction->setBankAccount(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Transaction.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=TransactionRepository::class)
+ */
+class Transaction
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=BankAccount::class, inversedBy="transactions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $bankAccount;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tireur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modeDePaiement;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $cheque;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $rib;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $debit;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $credit;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBankAccount(): ?BankAccount
+    {
+        return $this->bankAccount;
+    }
+
+    public function setBankAccount(?BankAccount $bankAccount): self
+    {
+        $this->bankAccount = $bankAccount;
+
+        return $this;
+    }
+
+    public function getModeDePaiement(): ?string
+    {
+        return $this->modeDePaiement;
+    }
+
+    public function setModeDePaiement(string $modeDePaiement): self
+    {
+        $this->modeDePaiement = $modeDePaiement;
+
+        return $this;
+    }
+
+    public function getCheque(): ?string
+    {
+        return $this->cheque;
+    }
+
+    public function setCheque(?string $cheque): self
+    {
+        $this->cheque = $cheque;
+
+        return $this;
+    }
+
+    public function getRib(): ?string
+    {
+        return $this->rib;
+    }
+
+    public function setRib(?string $rib): self
+    {
+        $this->rib = $rib;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDebit(): ?float
+    {
+        return $this->debit;
+    }
+
+    public function setDebit(?float $debit): self
+    {
+        $this->debit = $debit;
+
+        return $this;
+    }
+
+    public function getCredit(): ?float
+    {
+        return $this->credit;
+    }
+
+    public function setCredit(?float $credit): self
+    {
+        $this->credit = $credit;
+
+        return $this;
+    }
+
+    public function getTireur(): ?string
+    {
+        return $this->tireur;
+    }
+
+    public function setTireur(string $tireur): self
+    {
+        $this->tireur = $tireur;
+
+        return $this;
+    }
+}
